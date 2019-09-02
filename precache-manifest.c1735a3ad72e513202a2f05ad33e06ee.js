266f884742450544a2bd1be5152584b80d45e058
ryan <ryan.cu1200@gmail.com>
2019-09-02 18:32:23 +0530

    Updates

---
+++ precache-manifest.c1735a3ad72e513202a2f05ad33e06ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeecdc8d97de6fc84ea06a3c78b07857",
+    "url": "/roboclub/index.html"
+  },
+  {
+    "revision": "250cbc1a0a9b29b5f532",
+    "url": "/roboclub/static/css/main.ed631095.chunk.css"
+  },
+  {
+    "revision": "c258f7f510f6a1e4cd33",
+    "url": "/roboclub/static/js/2.d3410d48.chunk.js"
+  },
+  {
+    "revision": "250cbc1a0a9b29b5f532",
+    "url": "/roboclub/static/js/main.fe169ab6.chunk.js"
+  },
+  {
+    "revision": "364140ceba0fe81efe1d",
+    "url": "/roboclub/static/js/runtime~main.a5c34b16.js"
+  }
+]);
